e5657955ae1755d9ac5a7addfdffd5271e6c4deb
michalsiwiec123 <michalsiwiec2003@gmail.com>
2025-06-21 20:14:13 +0200

    Delete src directory

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router/router';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
-createApp(App).use(router).mount('#app');
