7320d402506e044bd9a36c4d66502e4bbf435f9e
michalsiwiec123 <michalsiwiec2003@gmail.com>
2025-06-21 19:57:24 +0200

    Add files via upload

---
+++ AppHeader2.vue
@@ -0,0 +1,17 @@
+/* eslint-disable vue/multi-word-component-names */
+<template>
+    <header class="header">
+      <h1>React Site</h1>
+      <div>logo</div>
+    </header>
+  </template>
+  
+  <style scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    border-bottom: 2px solid #ccc;
+  }
+  </style>
+  
